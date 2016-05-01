1a724a272d5a52508018366d759b85cc115390e1
lihaoda <lihaoda9@163.com>
2016-05-01 20:13:16 +0800

    fix bug

--- src/main/scala/edu/thu/cs/database/spark/rdd/RTreeRDD.scala
+++ src/main/scala/edu/thu/cs/database/spark/rdd/RTreeRDD.scala
@@ -98,7 +98,7 @@ object RTreeRDD {
             //rdd.logWarning(s"mbr for index ${index} not exist!");
         }
       }
-      SparkContext.getOrCreate().runJob[RTree[T, U], Rectangle](rdd, getPartitionMbr, rdd.partitions.indices, resultHandler);
+      SparkContext.getOrCreate().runJob(rdd, getPartitionMbr, rdd.partitions.indices, resultHandler);
       recArray
     }
   }
